48a1e1771ebac4b2cba77f11cfa84f3ef36a60bc
Alex Hirschfe ld <alex@d-qoi.com>
2018-12-28 00:13:10 -0600

    Finishing merge

--- custom/setup-wm.el
+++ custom/setup-wm.el
@@ -60,57 +60,57 @@
   :config
   (require 'exwm-config)
   :init
-  (progn
-    (require 'exwm-systemtray)
-    (exwm-systemtray-enable)
+  (require 'exwm-systemtray)
+  (exwm-systemtray-enable)
 
-    (require 'helm-exwm)
-    (global-set-key (kbd "<s-tab>") 'helm-exwm)
+  (require 'helm-exwm)
+  (global-set-key (kbd "<s-tab>") 'helm-exwm)
 
-    (use-package gpastel
-      :ensure t)
+  (add-hook 'exwm-edit-compose-hook 'ag-exwm/on-exwm-edit-compose)
 
-    (use-package desktop-environment
-      :ensure t
-      :init
-      (progn
-        (desktop-environment-mode)
-        (setq desktop-environment-volume-get-command "pactl list sinks | grep 'Volume: f' | cut -c30-34")
-        (setq desktop-environment-volume-toggle-command "pactl set-sink-mute 0 toggle")
-        (setq desktop-environment-volume-toggle-microphone-command "pactl set-source-mute 0 toggle")
-        (setq desktop-environment-screenshot-command "gnome-screenshot")
-        (setq desktop-environment-screenshot-partial-command "gnome-screenshot -a")
-        (setq desktop-environment-brightness-get-command "cat /sys/class/backlight/acpi_video0/brightness")
-        (setq  desktop-environment-brightness-get-regexp "\\([0-9]+\\)")
-        (define-key desktop-environment-mode-map (kbd "<XF86MonBrightnessUp>") nil)
-        (define-key desktop-environment-mode-map (kbd "<XF86MonBrightnessDown>") nil)
-        (define-key desktop-environment-mode-map (kbd "S-<XF86MonBrightnessUp>") nil)
-        (define-key desktop-environment-mode-map (kbd "S-<XF86MonBrightnessDown>") nil)))
+  (add-hook 'exwm-manage-finish-hook
+            (lambda ()
+              (when (and exwm-class-name
+                         (string= exwm-class-name "XTerm"))
+                (exwm-input-set-local-simulation-keys '(([?\C-c ?\C-c] . ?\C-c))))))
+  (require 'exwm-randr)
+  (setq exwm-randr-workspace-output-plist '(0 "VGA1"))
+  (defvar exwm-randr-workspace-output-plist)
+  (add-hook 'exwm-randr-screen-change-hook
+            (lambda ()
+              (start-process-shell-command
+               "xrandr" nil "xrandr --output VGA1 --left-of LVDS1 --auto")))
+  (exwm-randr-enable))
 
-    (use-package exwm-edit
-      :config
-      (require 'markdown-mode)
-      (defun ag-exwm/on-exwm-edit-compose ()
-        ;;(spacemacs/toggle-visual-line-navigation-on)
-        (markdown-mode))
+(use-package desktop-environment
+  :after exwm
+  :custom
+  (desktop-environment-volume-get-command "pactl list sinks | grep 'Volume: f' | cut -c30-34")
+  (desktop-environment-volume-toggle-command "pactl set-sink-mute 0 toggle")
+  (desktop-environment-volume-toggle-microphone-command "pactl set-source-mute 0 toggle")
+  (desktop-environment-screenshot-command "gnome-screenshot")
+  (desktop-environment-screenshot-partial-command "gnome-screenshot -a")
+  (desktop-environment-brightness-get-command "cat /sys/class/backlight/acpi_video0/brightness")
+  (desktop-environment-brightness-get-regexp "\\([0-9]+\\)")
+  :bind
+  (:map desktop-environment-mode-map
+        ("<XF86MonBrightnessUp>" . nil)
+        ("<XF86MonBrightnessDown>" . nil)
+        ("S-<XF86MonBrightnessUp>" . nil)
+        ("S-<XF86MonBrightnessDown>" . nil))
+  :config
+  (desktop-environment-mode))
 
-      (add-hook 'exwm-edit-compose-hook 'ag-exwm/on-exwm-edit-compose))
+(use-package gpastel
+  :after exwm)
 
-    (require 'exwm-randr)
-    (defvar exwm-randr-workspace-output-plist)
-    (setq exwm-randr-workspace-output-plist '(0 "VGA1"))
-    (add-hook 'exwm-randr-screen-change-hook
-              (lambda ()
-                (start-process-shell-command
-                 "xrandr" nil "xrandr --output VGA1 --left-of LVDS1 --auto")))
-
-    (add-hook 'exwm-manage-finish-hook
-              (lambda ()
-                (when (and exwm-class-name
-                           (string= exwm-class-name "XTerm"))
-                  (exwm-input-set-local-simulation-keys '(([?\C-c ?\C-c] . ?\C-c))))))
-
-    (exwm-randr-enable)))
+(use-package exwm-edit
+  :after exwm
+  :config
+  (require 'markdown-mode)
+  (defun ag-exwm/on-exwm-edit-compose ()
+    ;;(spacemacs/toggle-visual-line-navigation-on)
+    (markdown-mode)))
 
 (provide 'setup-wm)
 ;;; setup-wm.el ends here
--- init.el
+++ init.el
@@ -52,7 +52,10 @@
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
+ '(cscope-initial-directory "~/cscopedb/" t)
+ '(cscope-keymap-prefix "C-c M-s")
  '(custom-enabled-themes (quote (manoj-dark)))
+ '(dtrt-indent-verbosity0 nil t)
  '(ecb-options-version "2.50")
  '(elpy-modules
    (quote
@@ -60,9 +63,21 @@
  '(elpy-project-ignored-directories
    (quote
     (".tox" "build" "dist" ".cask" ".ipynb_checkpoints" "_out")))
+ '(flycheck-check-syntax-automatically (quote (save idle-change mode-enabled)))
+ '(flycheck-checker-error-threshold 10000)
+ '(flycheck-cppcheck-checks "all")
+ '(flycheck-python-pylint-executable "python2")
+ '(helm-gtags-auto-update t)
+ '(helm-gtags-ignore-case t)
+ '(helm-gtags-prefix-key "g")
+ '(helm-gtags-pulse-at-cursor t)
+ '(helm-gtags-suggested-key-mapping t)
+ '(helm-gtags-use-input-at-cursor t)
+ '(iedit-case-sensitive 1 t)
+ '(iedit-toggle-key-default nil t)
  '(package-selected-packages
    (quote
-    (zygospore helm-gtags helm yasnippet ws-butler volatile-highlights use-package undo-tree iedit dtrt-indent counsel-projectile company clean-aindent-mode anzu))))
+    (helm-exwm exwm-randr zygospore helm-gtags helm yasnippet ws-butler volatile-highlights use-package undo-tree iedit dtrt-indent counsel-projectile company clean-aindent-mode anzu))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
